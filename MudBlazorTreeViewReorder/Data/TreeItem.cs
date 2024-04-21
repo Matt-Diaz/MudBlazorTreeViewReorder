a5012851f5d76fa627e8302e6ffa74b650b605c4
Matt Diaz <diazlmatt.dev@gmail.com>
2024-04-21 17:18:25 -0600

    Add project files.

---
+++ MudBlazorTreeViewReorder/Data/TreeItem.cs
@@ -0,0 +1,32 @@
+﻿namespace MudBlazorTreeViewReorder.Data
+{
+    public class TreeItem
+    {
+        public TreeItem(string text, string parentId = null)
+        {
+            Text = text;
+            ParentID = parentId;
+            ID = GenerateID();
+        }
+        public string Text { get; set; }
+        public bool Expanded { get; set; }
+        public bool IsSelected { get; set; }
+        public string ParentID { get; set; }
+        public string ID { get; set; }
+        public DragDropAction DropAction { get; set; }
+
+        private string GenerateID()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+
+    public enum DragDropAction
+    {
+        None,
+        Above,
+        Inside,
+        Below,
+        Error
+    }
+}
